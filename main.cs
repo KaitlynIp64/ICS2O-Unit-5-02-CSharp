1a62d3e4ad23de4a2c4a5e77be8207221d4c40e0
KaitlynIp64 <99354960+KaitlynIp64@users.noreply.github.com>
2022-04-27 18:07:16 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,26 @@
+// Created by: Kaitlyn Ip
+// Created on: April 2022
+//
+// This program accepts user input
+
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int userInteger;
+        Console.WriteLine("Enter a number (positive or negative):");
+        userInteger = int.Parse(Console.ReadLine());
+        if (userInteger > -1)
+        {
+            userInteger = Convert.ToInt32(Console.Read());
+            Console.WriteLine("The integer you entered is positive.");
+            Console.ReadLine();
+        }
+        else
+        {
+            Console.WriteLine("The integer you entered is negative.");
+        }
+    }
+}
